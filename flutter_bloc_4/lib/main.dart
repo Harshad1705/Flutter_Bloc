2bf39e91f986c13551bf7868d2f0c761da867f81
Harshad Lande <harshadlandeco19@acropolis.in>
2023-08-26 20:43:51 +0530

    bloc to bloc communication

---
+++ flutter_bloc_4/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_4/cubit/counter_cubit.dart';
+import 'package:flutter_bloc_4/presentation/router/app_router.dart';
+
+import 'logic/cubit/internet_cubit.dart';
+
+void main() {
+  runApp(MyApp(
+    appRouter: AppRouter(),
+    connectivity: Connectivity(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final AppRouter appRouter;
+  final Connectivity connectivity;
+
+  const MyApp({
+    required this.appRouter,
+    required this.connectivity,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<InternetCubit>(
+          create: (context) => InternetCubit(connectivity: connectivity),
+        ),
+        BlocProvider<CounterCubit>(
+          create: (context) => CounterCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        onGenerateRoute: appRouter.onGenerateRoute,
+      ),
+    );
+  }
+}
